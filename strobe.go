2b1ed5522c7b804d5a72c4b72b306a8458a7fee1
Sudhir Jonathan <sudhir.j@gmail.com>
2015-01-22 01:15:35 +0530

    Changed locker to anon struct

--- strobe.go
+++ strobe.go
@@ -6,26 +6,26 @@ import "sync"
 type Strobe struct {
 	listeners map[chan string]bool
 	views     map[<-chan string]chan string
-	lock      sync.Locker
+	sync.RWMutex
 }
 
 //Listen creates a new receiver channel which acts as a subscription. In order to prevent leaks, always return a channel after use via `Forget`
 func (s *Strobe) Listen() <-chan string {
 	listener := make(chan string)
-	s.lock.Lock()
+	s.Lock()
 	s.listeners[listener] = true
 	s.views[listener] = listener
-	s.lock.Unlock()
+	s.Unlock()
 	return listener
 }
 
 //Pulse sends a message to all listening channels
 func (s *Strobe) Pulse(message string) {
-	s.lock.Lock()
+	s.Lock()
 	for c := range s.listeners {
 		c <- message
 	}
-	s.lock.Unlock()
+	s.Unlock()
 }
 
 //Count gives the number of listeners
@@ -35,13 +35,13 @@ func (s *Strobe) Count() int {
 
 //Off removes a channel from the list of receivers
 func (s *Strobe) Off(view <-chan string) {
-	s.lock.Lock()
+	s.Lock()
 	delete(s.listeners, s.views[view])
 	delete(s.views, view)
-	s.lock.Unlock()
+	s.Unlock()
 }
 
 //NewStrobe creates a new Strobe that can be used for PubSub
 func NewStrobe() *Strobe {
-	return &Strobe{listeners: make(map[chan string]bool), views: make(map[<-chan string]chan string), lock: &sync.Mutex{}}
+	return &Strobe{listeners: make(map[chan string]bool), views: make(map[<-chan string]chan string)}
 }
