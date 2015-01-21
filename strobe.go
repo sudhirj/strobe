6f5cd84172a203c419c272a0393839f0650ca41a
Sudhir Jonathan <sudhir.j@gmail.com>
2015-01-21 21:58:44 +0530

    Quick refactor

--- strobe.go
+++ strobe.go
@@ -10,30 +10,30 @@ type Strobe struct {
 }
 
 //Listen creates a new receiver channel which acts as a subscription. In order to prevent leaks, always return a channel after use via `Forget`
-func (p *Strobe) Listen() <-chan string {
+func (s *Strobe) Listen() <-chan string {
 	listener := make(chan string)
-	p.lock.Lock()
-	p.listeners[listener] = true
-	p.views[listener] = listener
-	p.lock.Unlock()
+	s.lock.Lock()
+	s.listeners[listener] = true
+	s.views[listener] = listener
+	s.lock.Unlock()
 	return listener
 }
 
 //Pulse sends a message to all listening channels
-func (p *Strobe) Pulse(message string) {
-	p.lock.Lock()
-	for c := range p.listeners {
+func (s *Strobe) Pulse(message string) {
+	s.lock.Lock()
+	for c := range s.listeners {
 		c <- message
 	}
-	p.lock.Unlock()
+	s.lock.Unlock()
 }
 
 //Off removes a channel from the list of receivers
-func (p *Strobe) Off(view <-chan string) {
-	p.lock.Lock()
-	delete(p.listeners, p.views[view])
-	delete(p.views, view)
-	p.lock.Unlock()
+func (s *Strobe) Off(view <-chan string) {
+	s.lock.Lock()
+	delete(s.listeners, s.views[view])
+	delete(s.views, view)
+	s.lock.Unlock()
 }
 
 //NewStrobe creates a new Strobe that can be used for PubSub
